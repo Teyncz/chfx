9770e0e1ef71360641437d309b09b628fcadeeb8
Teyncz <thibault.roth@etu.unistra.fr>
2025-04-27 21:28:57 +0200

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Cela ignore les erreurs ESLint pendant le build
+  },
 };
 
 export default nextConfig;
